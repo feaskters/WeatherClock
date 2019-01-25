c604b112c15338d68b5b0688226023c2daba9ee8
zmh <739296759@qq.com>
2019-01-25 10:14:39 +0800

    Initial Commit

---
+++ WeatherClock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherClock
+//
+//  Created by iOS123 on 2019/1/25.
+//  Copyright © 2019年 iOS123. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
