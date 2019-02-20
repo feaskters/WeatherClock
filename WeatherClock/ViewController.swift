bb856dac40f44fdca7ab9f0a2224ff5710bb8e03
zmh <739296759@qq.com>
2019-02-20 10:34:36 +0800

    return to the first page when click the tabbaritem

--- WeatherClock/ViewController.swift
+++ WeatherClock/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
 
-
+        //若是第一和第三个页面，点击tabitem时返回首页
+        if item.tag == 0 || item.tag == 2 {
+            (self.viewControllers![item.tag] as! UINavigationController).popToRootViewController(animated: true)
+        }
+    }
 }
 
